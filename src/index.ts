d72d0f918b37cecdee5059e10d0b4e8335453a30
shin902 <shinokabe7@outlook.jp>
2025-09-29 00:02:26 +0900

    コメントアウトを削除2（index.ts）

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,6 @@ import { Command } from "commander";
 const program = new Command();
 import { listFunc } from "./commands/list";
 
-// サブコマンドの定義
-
-// const main = defineCommand({
-//   meta: {
-//     name: "mcp-manager",
-//     description: "mcpを各エージェントに簡単に適用するCLIツールです。",
-//   },
-//   subCommands: {
-//     list: list,
-//   },
-// });
-
-// runMain(main);
-
 program.version("0.0.1", "-v, --version");
 
 program
