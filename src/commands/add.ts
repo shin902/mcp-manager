8455baca2ac33a70473dc8d14432b6d7e3f10d6e
shin902 <shinokabe7@outlook.jp>
2025-10-01 00:16:18 +0900

    jsonオブジェクトに追記して、ファイル書き出し機能を追加

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,12 +1,18 @@
+import { exportMCPSettings } from "../export-settings";
+import { importMCPSettings } from "../import-settings";
+
 export function addFunc(
 	name: string,
 	command: string,
 	args: string[],
 	force?: boolean,
-	config?: string[],
+	config?: string,
 	env?: string[],
 ) {
 	console.log(
 		`name: ${name}\ncommand: ${command}\nargs: ${args}\nforce: ${force}\nenv: ${env}\nconfig: ${config}`,
 	);
+	const obj = importMCPSettings(config ? config : undefined);
+	obj[name] = { command: command, args: args, env: env };
+	exportMCPSettings(obj, config ? config : undefined);
 }
