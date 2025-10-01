2b9b0a65bcb29258a40293f4c59a7e7443cffc77
shin902 <shinokabe7@outlook.jp>
2025-10-01 18:20:55 +0900

    余計な空行を削除

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -2,22 +2,21 @@ import { exportMCPSettings } from "../export-settings";
 import { importMCPSettings } from "../import-settings";
 
 export function addFunc(
-	name: string,
-	command: string,
-	args: string[],
-	force?: boolean,
-	config?: string,
-	env?: string[],
+  name: string,
+  command: string,
+  args: string[],
+  force?: boolean,
+  config?: string,
+  env?: string[],
 ) {
-	console.log(
-		`name: ${name}\ncommand: ${command}\nargs: ${args}\nforce: ${force}\nenv: ${env}\nconfig: ${config}`,
-	);
-	const obj = importMCPSettings(config ? config : undefined);
-	if (force || !(name in obj.mcpServers)) {
-		obj.mcpServers[name] = { command: command, args: args, env: env };
-		exportMCPSettings(obj, config ? config : undefined);
-	} else {
-		console.log("すでに同じ名前のMCPサーバーが存在します");
-	}
-
+  console.log(
+    `name: ${name}\ncommand: ${command}\nargs: ${args}\nforce: ${force}\nenv: ${env}\nconfig: ${config}`,
+  );
+  const obj = importMCPSettings(config ? config : undefined);
+  if (force || !(name in obj.mcpServers)) {
+    obj.mcpServers[name] = { command: command, args: args, env: env };
+    exportMCPSettings(obj, config ? config : undefined);
+  } else {
+    console.log("すでに同じ名前のMCPサーバーが存在します");
+  }
 }
