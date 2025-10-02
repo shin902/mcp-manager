4d903a6586df95c864c1e87ad7ca00496503745a
shin902 <shinokabe7@outlook.jp>
2025-10-02 23:10:27 +0900

    bun lint これ必要ある？

--- src/import-settings.ts
+++ src/import-settings.ts
@@ -2,19 +2,20 @@ import { existsSync, readFileSync } from "node:fs";
 import { homedir } from "node:os";
 import { join } from "node:path";
 
-export function importMCPSettings(pathfromhomedir: string = ".mcp-manager.json") {
-	const filePath = join(homedir(), pathfromhomedir);
-	if (!existsSync(filePath)) {
-		throw new Error("ファイルが存在しません");
-	}
-	const jsonString = readFileSync(filePath, "utf8");
-	const obj = JSON.parse(jsonString);
-	if (!("mcpServers" in obj)) {
-		throw new Error("mcpServersが存在しません");
-	}
-	if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
-		throw new Error("無効なJSON形式");
-	}
-
-	return obj;
+export function importMCPSettings(
+  pathfromhomedir: string = ".mcp-manager.json",
+) {
+  const filePath = join(homedir(), pathfromhomedir);
+  if (!existsSync(filePath)) {
+    throw new Error("ファイルが存在しません");
+  }
+  const jsonString = readFileSync(filePath, "utf8");
+  const obj = JSON.parse(jsonString);
+  if (!("mcpServers" in obj)) {
+    throw new Error("mcpServersが存在しません");
+  }
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    throw new Error("無効なJSON形式");
+  }
+  return obj;
 }
