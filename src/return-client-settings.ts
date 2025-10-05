65b4d912da0af9a8e9d9765e0df47ca87fa41b20
shin902 <shinokabe7@outlook.jp>
2025-10-05 23:27:41 +0900

    client名からそのhomedirからのパスを取得する関数を追加

---
+++ src/return-client-settings.ts
@@ -0,0 +1,27 @@
+export function getPathFromClientName(client: string = ""): string {
+  let pathfromhomedir: string = ".mcp-manager.json";
+  switch (client) {
+    case "claude-code": {
+      pathfromhomedir = ".claude.json";
+      break;
+    }
+    case "gemini-cli": {
+      pathfromhomedir = ".gemini/settings.json";
+      break;
+    }
+    case "claude": {
+      pathfromhomedir =
+        "Library/Application Support/Claude/claude_desktop_config.json";
+      break;
+    }
+    case undefined:
+    case "": {
+      // デフォルトの.mcp-manager.jsonを使用
+      break;
+    }
+    default: {
+      throw new Error("無効なクライアント名です");
+    }
+  }
+  return pathfromhomedir;
+}
