f59a7e59d253713dcdb854cd8211b2bebf736243
shin902 <shinokabe7@outlook.jp>
2025-10-05 19:47:12 +0900

    インデントをスペースに変更

--- src/import-settings.ts
+++ src/import-settings.ts
@@ -1,17 +1,16 @@
 import { existsSync, readFileSync } from "node:fs";
 import { homedir } from "node:os";
 import { join } from "node:path";
-import type { Config } from "./schemas";
-import { ConfigSchema } from "./schemas";
+import { ConfigSchema, type Config } from "./schemas";
 
 export function importMCPSettings(
-	pathfromhomedir: string = ".mcp-manager.json",
+  pathfromhomedir: string = ".mcp-manager.json",
 ) {
-	const filePath = join(homedir(), pathfromhomedir);
-	if (!existsSync(filePath)) {
-		throw new Error("ファイルが存在しません");
-	}
-	const jsonString = readFileSync(filePath, "utf8");
-	const obj: Config = ConfigSchema.parse(JSON.parse(jsonString));
-	return obj;
+  const filePath = join(homedir(), pathfromhomedir);
+  if (!existsSync(filePath)) {
+    throw new Error("ファイルが存在しません");
+  }
+  const jsonString = readFileSync(filePath, "utf8");
+  const obj: Config = ConfigSchema.parse(JSON.parse(jsonString));
+  return obj;
 }
