59d00675f8b19111ae5dfdfcce08ebb2081064fc
shin902 <shinokabe7@outlook.jp>
2025-10-08 23:21:09 +0900

    removeコマンドでちゃんとサーバーが存在するか検証

--- src/commands/remove.ts
+++ src/commands/remove.ts
@@ -7,6 +7,10 @@ export function removeFunc(server: string, app: string) {
   const filePath = getPathFromAppName(app);
 
   const obj: Config = importMCPSettings(filePath);
-  delete obj.mcpServers[server];
+  if (server in obj.mcpServers) {
+    delete obj.mcpServers[server];
+  } else {
+    console.log(`Server ${server} not found`);
+  }
   exportMCPSettings(obj, filePath);
 }
