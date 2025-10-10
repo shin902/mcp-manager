cfde31868eb0e00e32b1979e078db0d224be5d14
shin902 <shinokabe7@outlook.jp>
2025-10-10 19:06:27 +0900

    settings.tsを入れ替え

--- src/settings.ts
+++ src/settings.ts
@@ -6,8 +6,33 @@ import { join } from "node:path";
 import { parse } from "secure-json-parse"
 import { existsSync } from "node:fs";
 
+
+const APP_PATHS: Record<string, string> = {
+    "claude-code": ".claude.json",
+    "gemini-cli": ".gemini/settings.json",
+    "claude-desktop":
+        "Library/Application Support/Claude/claude_desktop_config.json",
+};
+
 const defaultJson = {
     mcpServers: {}
+}
+
+const DEFAULT_PATH = ".mcp-manager.json";
+
+
+export function getPathFromAppName(app: string = ""): string {
+    let appPath = APP_PATHS[app];
+
+    if (app === "") {
+        appPath = DEFAULT_PATH;
+    } else if (!(app in APP_PATHS) || !appPath) {
+        throw new Error("無効なクライアント名です");
+    }
+
+    const path = appPath;
+
+    return join(homedir(), path);
 }
 
 export function importMCPSettings(filePath: string = ".mcp-manager.json") {
@@ -45,27 +70,4 @@ export function exportMCPSettings(
 
     const formatterJson = JSON.stringify(result.data, null, 2);
     writeFileSync(filePath, formatterJson);
-}
-
-const APP_PATHS: Record<string, string> = {
-    "claude-code": ".claude.json",
-    "gemini-cli": ".gemini/settings.json",
-    "claude-desktop":
-        "Library/Application Support/Claude/claude_desktop_config.json",
-};
-
-const DEFAULT_PATH = ".mcp-manager.json";
-
-export function getPathFromAppName(app: string = ""): string {
-    let appPath = APP_PATHS[app];
-
-    if (app === "") {
-        appPath = DEFAULT_PATH;
-    } else if (!(app in APP_PATHS) || !appPath) {
-        throw new Error("無効なクライアント名です");
-    }
-
-    const path = appPath;
-
-    return join(homedir(), path);
 }
